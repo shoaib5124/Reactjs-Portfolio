d265946a413bec57cac56b84ef07824dbe89a815
Shoaib Ul Hassan <shoaibulhasan556@gmail.com>
2025-08-08 20:39:53 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-red-700 font-bold'>Hello Shoaib </h1>
+      <h1 className='text-red-700 font-bold'>Hello Shoaib </h1>
+      <h1 className='text-red-700 font-bold'>Hello Shoaib </h1>
+      <h1 className='text-red-700 font-bold'>Hello Shoaib </h1>
+      <h1 className='text-red-700 font-bold'>Hello Shoaib </h1>
+    </>
+  )
+}
+
+export default App
