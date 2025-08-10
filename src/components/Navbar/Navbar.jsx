7bfdddc4b01bd66093f1d31afad6d5adb1e7936d
Shoaib Ul Hassan <shoaib@example.com>
2025-08-10 13:03:25 +0500

    built project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const handleMenuItemClick =(sectionId)=>{
     }
 }
 // menuItem to run loop 
-const menuItem = [
+const menuItems = [
     {id:"about", label:"About"},
     {id:"skills", label:"Skills"},
     {id:"work", label:"Projects"},
@@ -38,7 +38,44 @@ const menuItem = [
 
   return (
     <div>
-      <nav className='bg-white'>
+
+      <nav className={`top-0 flex fixed z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] 
+        ${isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md":"bg-transparent"}`}>
+        
+       
+        <div className='text-white flex py-5 justify-between items-center'>
+
+            {/* Logo */}
+            <div className='lg:font-bold font-semibold'>
+                <span className='text-[#8245ec]'>&lt;</span>
+                <span className='text-white'>Shoaib Ul</span>
+                <span className='text-[#8245ec]'>/</span>
+                <span className='text-white'>Hassan</span>
+                <span className='text-[#8245ec]'>&gt;</span>
+            </div>
+
+           {/* Deskstop Menu */}
+           <ul className='hidden md:flex space-x-8 text-gray-300'>
+                {menuItems.map((item)=>(
+                    <li
+                    key={item.id} 
+                    className={`cursor-pointer hover:text-[rgb(130,69,236)]
+                    ${isSectionId == item.id ? "text-[#8245ec]": "" 
+                    }`}
+                    >
+                        <button onClick={()=> handleMenuItemClick(item.id)}>
+                            {item.label}
+                        </button>
+                    </li>
+                ))} 
+           </ul>
+
+           {/* Social Icons */}
+           <div className='hidden md:flex space-x-4'>
+             <a><FaGithub size={24}/></a>
+             <a><FaLinkedin size={24}/></a>
+            </div>       
+        </div>  
       </nav>
     </div>
   )
