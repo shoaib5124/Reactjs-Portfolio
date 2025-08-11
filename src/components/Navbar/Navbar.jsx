2b5f4a9a34e93944d874295d3864bdf4f99d8dea
Shoaib Ul Hassan <shoaibulhasan556@gmail.com>
2025-08-11 09:04:41 +0500

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,84 +1,149 @@
-import React, {useState, useEffect} from 'react';
-import {FiMenu, FiX} from 'react-icons/fi';
-import {FaGithub, FaLinkedin} from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Navbar = () => {
-  const [isOpen,setIsOpen]= useState(false);        
-  const [isScrolled,setIsScrolled]= useState(false);        
-  const [isSectionId,setSectionId]= useState("");
-  
-//A function to trace the scrolling to change the Navbar background
-useEffect(()=>{
-const handleScroll = ()=>{
-    setIsScrolled(window.scrollY>50)
-};
-window.addEventListener('scroll',handleScroll);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isSectionId, setSectionId] = useState("");
 
-return ()=>
-    window.removeEventListener('scroll',handleScroll)
-},[])
+  //A function to trace the scrolling to change the Navbar background
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50)
+    };
+    window.addEventListener('scroll', handleScroll);
 
-// Smooth scroll function
-const handleMenuItemClick =(sectionId)=>{
+    return () =>
+      window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  // Smooth scroll function
+  const handleMenuItemClick = (sectionId) => {
     setSectionId(sectionId);
     setIsOpen(false);
     const section = document.getElementById(sectionId);
-    if(section){
-        section.scrollIntoView({behavior:'smooth  '})
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth  ' })
     }
-}
-// menuItem to run loop 
-const menuItems = [
-    {id:"about", label:"About"},
-    {id:"skills", label:"Skills"},
-    {id:"work", label:"Projects"},
-    {id:"experience", label:"Expereince"},
-    {id:"education", label:"Education"}
-]
+  }
+  // menuItem to run loop 
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "work", label: "Projects" },
+    { id: "experience", label: "Expereince" },
+    { id: "education", label: "Education" }
+  ]
 
   return (
     <div>
 
-      <nav className={`top-0 flex fixed z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] 
-        ${isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md":"bg-transparent"}`}>
-        
-       
-        <div className='text-white flex py-5 justify-between items-center'>
+      <nav className={`top-0 flex w-full fixed z-50 transition duration-300  ] 
+        ${isScrolled ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md" : "bg-transparent"}`}>
 
-            {/* Logo */}
-            <div className='lg:font-bold font-semibold'>
-                <span className='text-[#8245ec]'>&lt;</span>
-                <span className='text-white'>Shoaib Ul</span>
-                <span className='text-[#8245ec]'>/</span>
-                <span className='text-white'>Hassan</span>
-                <span className='text-[#8245ec]'>&gt;</span>
-            </div>
+        <div className='text-white  px-10 w-full max-w-7xl md:mx-auto flex py-5 justify-between items-center'>
 
-           {/* Deskstop Menu */}
-           <ul className='hidden md:flex space-x-8 text-gray-300'>
-                {menuItems.map((item)=>(
-                    <li
-                    key={item.id} 
-                    className={`cursor-pointer hover:text-[rgb(130,69,236)]
-                    ${isSectionId == item.id ? "text-[#8245ec]": "" 
-                    }`}
-                    >
-                        <button onClick={()=> handleMenuItemClick(item.id)}>
-                            {item.label}
+          {/* Logo */}
+          <div className='text-xl md:text-2xl lg:text-3xl font-semibold lg:font-bold'>
+            <span className='text-[#8245ec]'>&lt;</span>
+            <span className='text-white'>Shoaib</span>
+            <span className='text-[#8245ec]'>/</span>
+            <span className='text-white'>Hassan</span>
+            <span className='text-[#8245ec]'>&gt;</span>
+          </div>
+
+          {/* Deskstop Menu */}
+          <ul className='hidden md:flex md:text-base lg:text-lg font-medium space-x-8 text-gray-300'>
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className={`cursor-pointer hover:text-[rgb(130,69,236)]
+                    ${isSectionId == item.id ? "text-[#8245ec]" : ""
+                  }`}
+              >
+                <button onClick={() => handleMenuItemClick(item.id)}>
+                  {item.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          {/* Social Icons */}
+          <div className='hidden md:flex space-x-4'>
+            <a
+              href='https://github.com/shoaib5124/Reactjs-Portfolio.git'
+              target='_blank'
+              rel='noopener noreferer'
+              className='text-gray-300 hover:text-[#8245ec]'
+            >
+              <FaGithub size={24} />
+            </a>
+            <a
+              href='https://www.linkedin.com/in/shoaib-ul-hassan-070ab333b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
+              target='_blank'
+              rel='noopener noreferer'
+              className='text-gray-300 hover:text-[#8245ec]'
+            >
+              <FaLinkedin size={24} />
+            </a>
+          </div>
+        </div>
+
+        {/* Mobile menu icons */}
+        <div className='md:hidden'>
+          {isOpen ?(
+              <FiX
+                className='text-3xl mt-5 mr-10 text-white cursor-pointer'
+                onClick={()=>setIsOpen(false)}
+              />
+            ):(
+              <FiMenu
+                className='text-3xl mt-5 mr-10 text-white cursor-pointer'
+                onClick={()=>setIsOpen(true)}
+              />
+            )}{/* Mobile Menu Items */}
+              {isOpen && (
+                <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden">
+                  <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+                    {menuItems.map((item) => (
+                      <li
+                        key={item.id}
+                        className={`cursor-pointer hover:text-white ${
+                          isSectionId === item.id ? "text-[#8245ec]" : ""
+                        }`}
+                      >
+                        <button onClick={() => handleMenuItemClick(item.id)}>
+                          {item.label}
                         </button>
-                    </li>
-                ))} 
-           </ul>
+                      </li>
+                    ))}
+                    <div className="flex space-x-4">
+                      <a
+                        href="https://github.com/codingmastr"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-300 hover:text-white"
+                      >
+                        <FaGithub size={24} />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/tarun-kaushik-553b441a4"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-300 hover:text-white"
+                      >
+                        <FaLinkedin size={24} />
+                      </a>
+                    </div>
+                  </ul>
+                </div>
+              )}
 
-           {/* Social Icons */}
-           <div className='hidden md:flex space-x-4'>
-             <a><FaGithub size={24}/></a>
-             <a><FaLinkedin size={24}/></a>
-            </div>       
-        </div>  
+        </div>
       </nav>
     </div>
   )
 }
-  
+
 export default Navbar
