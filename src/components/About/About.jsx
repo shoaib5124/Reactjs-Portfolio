2012c68fa8d29081c532d64ca8402d6e00079ca4
Shoaib Ul Hassan <shoaibulhasan556@gmail.com>
2025-08-11 10:05:19 +0500

    second commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ReactTypingEffect from 
 const About = () => {
   return (
     <section
@@ -11,8 +11,13 @@ const About = () => {
           {/* Greeting */}
           <h1>Hi, I am</h1>
           {/* Name */}
-          <h1>Shoaib Ul Hassan</h1>
+          <h2>Shoaib Ul Hassan</h2>
           {/* Skills headig with typing effects */}
+          <h3> 
+            <span>I am a 
+               
+            </span>
+          </h3>
         </div>
         <div></div>
       </div>
