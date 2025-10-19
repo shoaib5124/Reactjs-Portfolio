7c9a43f3276629f6df1781b06e91460356f7e4cf
Shoaib Ul Hassan <shoaib@example.com>
2025-10-20 00:00:34 +0500

    About text change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,74 +1,81 @@
 import React from 'react';
-import {Typewriter}from 'react-simple-typewriter';
+import { Typewriter } from 'react-simple-typewriter';
 import profileImage from '../../assets/profile2.png';
 import Container from '../container';
-import {Tilt} from 'react-tilt';
+import { Tilt } from 'react-tilt';
 const About = () => {
   return (
     <section
-    id="about"
-    className=' mt-16 md:mt-24 lg:mt-32 font-sans'
+      id="about"
+      className=' mt-16 md:mt-24 lg:mt-32 font-sans'
     >
       <Container className='md:flex-row md:gap-x-10 flex flex-col-reverse gap-y-8  items-center justify-between py-10 '>
 
-      {/* <!-- Glowing blue light --> */}
-     <div class="absolute top-32 left-64 w-72 h-72 bg-[#8245ec] rounded-full blur-3xl opacity-30 animate-pulse"></div>
+        {/* <!-- Glowing blue light --> */}
+        <div class="absolute top-32 left-64 w-72 h-72 bg-[#8245ec] rounded-full blur-3xl opacity-30 animate-pulse"></div>
 
-            {/* Left text side */}
-            <div className=' w-full text-center md:text-left md:w-1/2'>
-                {/* Greeting */}
-                <h1 className='text-3xl sm:text-4xl  font-bold md:text-5xl lg:text-7xl mb-2'>Hi, I am</h1>
-                {/* Name */}
-                <h2 className='text-4xl sm:text-5xl font-bold md:text-6xl lg:text-8xl'>Shoaib Ul Hassan</h2>
-                {/* Skills headig with typing effects */}
-                <h3 className='text-3xl sm:text-4xl text-[#8245ec] font-bold md:text-5xl lg:text-7xl mb-2'> 
-                    <span className='text-white'>I am a </span>
-                    <Typewriter
-                        words={[
-                            'React. js Developer',
-                            'Frontend web Developer',
-                            'Mern Stack Developer',
-                            'Responsive Design Expert'
-                        ]}
-                        loop={0}
-                        cursor
-                        cursorStyle='|'
-                        typeSpeed={80}
-                        deleteSpeed={30}
-                        delaySpeed={1000}
-                    />
-                </h3>
-                {/* About me paragraph */}
-                <p className='text-base sm:text-lg md:text-lg lg:text-2xl text-gray-400 mt-8'>
-                  I’m a passionate Mern Stack Developer with 1.5 years of experience creating clean, user-friendly websites. Skilled in React.js, Tailwind CSS,Bootstrap,Node.js,Express.js,python,FastApi and modern JavaScript, I transform ideas into interactive digital experiences. I love learning, solving challenges, and building projects that make an impact about both visual appeal and functionality.
-                </p>
-                {/* Resume Button */}
-                <a 
-                href=''
-                target='_blank'
-                rel='noopener noreferer'
-                className='text-lg inline-block rounded-full py-3 mt-10 font-bold transition duration-300 transform hover:scale-105 px-8'
-                style={{
-                  background:'linear-gradient(90deg,#8245ec,#a855f7)',
-                  boxShadow:' 0 0 1px #8245ec, 0 0 1px #a855f7, 0 0 20px #8245ec '
-                }}
-                >
-                 DOWNLOAD CV
-                </a>
-            </div>
-            {/* Right image side */}
-            <div className='md:w-1/2 flex justify-center md:justify-end '>
-                <Tilt
-                className='w-48 h-48 sm:w-64 sm:h-64 md:w-[22rem] md:h-[30rem] lg:w-[27rem] lg:h-[35rem] border-4 border-purple-700 rounded-full '
-                >
-                  <img 
-                   src={profileImage}
-                   className='w-full h-full rounded-full object-cover'
-                  />  
-                </Tilt>     
-            </div>
+        {/* Left text side */}
+        <div className=' w-full text-center md:text-left md:w-1/2'>
+          {/* Greeting */}
+          <h1 className='text-3xl sm:text-4xl  font-bold md:text-5xl lg:text-7xl mb-2'>Hi, I am</h1>
+          {/* Name */}
+          <h2 className='text-4xl sm:text-5xl font-bold md:text-6xl lg:text-8xl'>Shoaib Ul Hassan</h2>
+          {/* Skills headig with typing effects */}
+          <h3 className='text-3xl sm:text-4xl text-[#8245ec] font-bold md:text-5xl lg:text-7xl mb-2'>
+            <span className='text-white'>I am a </span>
+            <Typewriter
+              words={[
+                'React. js Developer',
+                'Frontend web Developer',
+                'Mern Stack Developer',
+                'Responsive Design Expert'
+              ]}
+              loop={0}
+              cursor
+              cursorStyle='|'
+              typeSpeed={80}
+              deleteSpeed={30}
+              delaySpeed={1000}
+            />
+          </h3>
+          {/* About me paragraph */}
+          <p className='text-base sm:text-lg md:text-lg lg:text-2xl text-gray-400 mt-8'>
+            I’m a passionate Mern Stack Developer
+             with 1.5 years of experience creating 
+             clean,user-friendly websites. Skilled in
+             React.js, Tailwind CSS,Bootstrap,Node.js,
+             Express.js,python,FastApi and modern JavaScript,
+             I transform ideas into interactive digital experiences.
+             I love learning, solving challenges, and building projects 
+             that make an impact about both visual appeal and functionality.
+          </p>
+         {/* Resume Button */}
+          <a
+            href=''
+            target='_blank'
+            rel='noopener noreferer'
+            className='text-lg inline-block rounded-full py-3 mt-10 font-bold transition duration-300 transform hover:scale-105 px-8'
+            style={{
+              background: 'linear-gradient(90deg,#8245ec,#a855f7)',
+              boxShadow: ' 0 0 1px #8245ec, 0 0 1px #a855f7, 0 0 20px #8245ec '
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+        {/* Right image side */}
+        <div className='md:w-1/2 flex justify-center md:justify-end '>
+          <Tilt
+            className='w-48 h-48 sm:w-64 sm:h-64 md:w-[22rem] md:h-[30rem] lg:w-[27rem] lg:h-[35rem] border-4 border-purple-700 rounded-full '
+          >
+            <img
+              src={profileImage}
+              className='w-full h-full rounded-full object-cover'
+            />
+          </Tilt>
+        </div>
       </Container>
-       
+
     </section>
   )
 }
