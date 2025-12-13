0ba679881bc829a8d9800e89fa20642b60846c43
Shoaib Ul Hassan <shoaib@example.com>
2025-12-13 18:25:07 +0500

    Fix YouTube logo path and add correct image

--- src/constants.js
+++ src/constants.js
@@ -46,7 +46,7 @@ import vpsLogo from './assets/education_logo/vps_logo.png';
 // Project Section Logo's
 import password_generaterlogo from './assets/work_logoe/passwordlogo.png';
 import uniLogo from './assets/work_logoe/uni_logo.png';
-import YouTubeCloneLogo from './assets/work_logoe/YouTubeLogo.png';
+import YouTubeCloneLogo from './assets/work_logoe/Youtube_logo.png';
 import WeatherAppLogo from './assets/work_logoe/weather.png';
 
 export const SkillsInfo = [
